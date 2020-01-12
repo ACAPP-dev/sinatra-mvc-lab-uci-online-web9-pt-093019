64a7b38e7106121451c6c6508409cb33335989c3
Andrew Capp <acapp909@gmail.com>
2020-01-12 23:10:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    erb :user_input.erb
+    erb :user_input
   end
 
   post '/' do
     @pig_latin = PigLatinizer.new(params[:string])
-    erb :show_result.erb
+    erb :show_result
   end
 end
