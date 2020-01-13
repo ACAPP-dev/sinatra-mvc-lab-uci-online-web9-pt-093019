200d1d4ae3a156293eca6ab9f3df1da458cc0204
Andrew Capp <acapp909@gmail.com>
2020-01-13 00:06:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,11 +13,11 @@ class PigLatinizer
       text_array = word.split(//)
       if word.match?(/^[aeiou]/)
         word + "way"
-      elsif text.match?(/^[^aeiou]{3}/)
+      elsif word.match?(/^[^aeiou]{3}/)
         p text_array[3..-1].join() + text_array[0..2].join + "ay"
-      elsif text.match?(/^[^aeiou]{2}/)
+      elsif word.match?(/^[^aeiou]{2}/)
         p text_array[2..-1].join() + text_array[0..1].join + "ay"
-      elsif text.match?(/^[^aeiou]{1}/)
+      elsif word.match?(/^[^aeiou]{1}/)
         p  text_array[1..-1].join() + text_array[0] + "ay"
       end
     end
